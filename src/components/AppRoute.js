681e7b1050ac3a67acac36fa66f446650332cce2
dufva <sussiedufva@hotmail.com>
2021-12-20 17:41:29 +0100

    First commit, project setup

---
+++ src/components/AppRoute.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Route, BrowserRouter as Router, Routes} from "react-router-dom";
+
+function AppRoute() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default AppRoute
+
