e4f67c9816309afdb2d33b7ace81c7d34d2e5609
dufva <sussiedufva@hotmail.com>
2021-12-21 13:59:45 +0100

    ground structure added

--- src/components/AppRoute.js
+++ src/components/AppRoute.js
@@ -1,11 +1,23 @@
 import React from 'react'
 import {Route, BrowserRouter as Router, Routes} from "react-router-dom";
+import Home from './Landingpage'
+import Store from './Store'
+import About from './About'
+import Navbar from './Navbar'
 
 function AppRoute() {
     return (
-        <div>
-            
-        </div>
+        <>
+            <Router>
+                <Navbar/>
+
+                <Routes>
+                    <Route path='/' element={<Home/>}/>
+                    <Route path='/store' element={<Store/>}/>
+                    <Route path='/about' element={<About/>}/>
+                </Routes>
+            </Router>  
+        </>
     )
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+function Navbar() {
+    return (
+        <>  
+            <div className="h-16 w-full bg-black bg-opacity-50">
+                <div className="w-full h-full flex justify-center items-center">
+                    <div className="flex h-full items-center  hover:bg-black hover:bg-opacity-50">
+                        <div className="mx-4 text-white">Opcion</div>
+                        <div className=" h-8 w-px bg-gray-300"></div>
+                    </div>
+                    <div className="flex h-full items-center  hover:bg-black hover:bg-opacity-50">
+                        <div className="mx-4 text-white">Opcion</div>
+                        <div className=" h-8 w-px bg-gray-300"></div>
+                    </div>
+                    <div className="flex h-full items-center  hover:bg-black hover:bg-opacity-50">
+                        <div className="mx-4 text-white">Opcion</div>
+                        <div className=" h-8 w-px bg-gray-300"></div>
+                    </div>
+                    <div className="flex h-full items-center  hover:bg-black hover:bg-opacity-50">
+                        <div className="mx-4 text-white">Opcion</div>
+                        <div className=" h-8 w-px bg-gray-300"></div>
+                    </div>
+                    <div className="flex h-full  items-center hover:bg-black hover:bg-opacity-50">
+                        <div className="mx-4 text-white">Opcion</div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Navbar
