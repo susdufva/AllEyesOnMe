19069774bcaec2a02c78e36a8cf57cfa706c1ad1
dufva <sussiedufva@hotmail.com>
2021-12-28 16:26:14 +0100

    fetch products thru woocommerce and past to productcard with props

--- src/components/Landingpage.js
+++ src/components/Landingpage.js
@@ -6,7 +6,7 @@ function Landingpage() {
         <body>
             <div className="flex justify-center items-center mt-2">
                 <div className="lg:w-1/4 md:w-1/2 w-2/3">
-                    <img src={bgimage} alt="homepage image" />
+                    <img src={bgimage} alt="homepage" />
                 </div> 
                 <div className="">
                     <h3 className=""></h3>
--- src/components/Store.js
+++ src/components/Store.js
@@ -1,40 +1,56 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from "react";
 import WooCommerceRestApi from "@woocommerce/woocommerce-rest-api";
+import ProductCard from "./ProductCard";
+
+//get products from woocommerce with API
+const api = new WooCommerceRestApi({ 
+  url: "http://localhost/AllEyesOnMe/wordpress/",
+  consumerKey: process.env.REACT_APP_CUSTOMERKEY,
+  consumerSecret: process.env.REACT_APP_CUSTOMERSECRET,
+  version: "wc/v3",
+});
+
+console.log("api", api);
 
 function Store() {
 
-    const api = new WooCommerceRestApi({
-        url: "http://localhost/AllEyesOnMe/wordpress/",
-        consumerKey: process.env.REACT_APP_CUSTOMERKEY,
-        consumerSecret: process.env.REACT_APP_CUSTOMERSECRET,
-        version: "wc/v3"
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    //useEffect to fetch API
+    fetchProducts();
+  }, []);
+
+  let fetchProducts = () => {
+    api
+      .get("products", {
+        per_page: 10,
+      })
+      .then((response) => {
+        if (response.status === 200) {
+          setProducts(response.data);
+          console.log("RES", response.data);
+        }
+      })
+      .catch((error) => {
+        console.error("error", error);
       });
+  };
 
-    const [products, setProducts] = useState([]);
-
-    useEffect(() => {
-        fetchProducts();
-      }, []);
-    
-      let fetchProducts = () => {
-        api
-          .get("products", {
-            per_page: 10,
-          })
-          .then((response) => {
-            if (response.status === 200) {
-              setProducts(response.data);
-              console.log("RES", response.data);
-            }
-          })
-          .catch((error) => {});
-      };
-
-    return (
-        <div>
+  return (
+    <>
+      <div className="grid grid-cols-3 gap-1 mt-4">
         
-        </div>
-    )
+      {products.map((product)=>{
+        const firstImageSrc = product.images[0] && product.images[0].src ? product.images[0].src : '';
+        return (
+          <ProductCard key={product.id} productId={product.id} productName={product.name} imageSrc={firstImageSrc} price={product.price} categories= {product.categories} /> 
+                )    
+      }) }
+
+      </div>
+    </>
+  );
 }
 
-export default Store
+export default Store;
